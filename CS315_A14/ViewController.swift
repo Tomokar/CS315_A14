bc83e443ac0d40475b14a2f07d19e9de25e68e09
tomokar <93736469+Tomokar@users.noreply.github.com>
2022-12-09 14:01:18 -0600

    Initial Commit

---
+++ CS315_A14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS315_A14
+//
+//  Created by Kai X on 12/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
